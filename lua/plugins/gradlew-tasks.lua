b5c2a1bd20cc7e8890cbc39aae8c08e7f6bbdf1a
anbit <antreasmptzls5@gmail.com>
2025-06-29 15:12:05 +0200

    Update gradlew-tasks

--- lua/plugins/gradlew-tasks.lua
+++ lua/plugins/gradlew-tasks.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {--[[ things you want to change go here]]
+    },
+  },
+  {
     "aBit19/gradlew-tasks.nvim",
   },
 }
